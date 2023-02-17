3c13a97feb730b02691a0326f249fd40d6ff9a89
iumyxF <1119759672@qq.com>
2023-02-17 15:41:54 +0800

    feat(i18): ✨ 在18n基础上添加Validator校验

---
+++ spring-boot-i18n/src/main/java/com/example/language/controller/ValidatorController.java
@@ -0,0 +1,56 @@
+package com.example.language.controller;
+
+import com.example.language.entities.Result;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @description: 校验测试
+ * @Date 2023/2/17 14:46
+ * @Author fzy
+ */
+@Validated
+@RestController
+@RequestMapping("/demo/i18n")
+public class ValidatorController {
+
+    /**
+     * Validator 校验国际化
+     * 不传值 分别查看异常返回
+     * <p>
+     * 测试使用 not.null
+     */
+    @GetMapping("/test1")
+    public Result<Void> test1(@NotBlank(message = "{not.null}") String str) {
+        return Result.ok(str);
+    }
+
+    /**
+     * Bean 校验国际化
+     * 不传值 分别查看异常返回
+     * <p>
+     * 测试使用 not.null
+     */
+    @GetMapping("/test2")
+    public Result<TestI18nBo> test2(@Validated TestI18nBo bo) {
+        return Result.ok(bo);
+    }
+
+    @Data
+    public static class TestI18nBo {
+
+        @NotBlank(message = "name {not.null}")
+        private String name;
+
+        @NotNull(message = "{not.null}")
+        @Range(min = 0, max = 100, message = "age {length.not.valid}")
+        private Integer age;
+    }
+}
--- spring-boot-i18n/src/main/java/com/example/language/exception/GlobalExceptionHandler.java
+++ spring-boot-i18n/src/main/java/com/example/language/exception/GlobalExceptionHandler.java
@@ -2,15 +2,22 @@ package com.example.language.exception;
 
 import com.example.language.entities.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.stream.Collectors;
 
 /**
  * @author feng
  * @date 2023/2/9 20:44
  */
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CommonException.class)
@@ -18,5 +25,40 @@ public class GlobalExceptionHandler {
         log.error(e.getMessage(), e);
         return Result.fail(e.getMessage());
     }
+
+    /**
+     * 自定义验证异常
+     */
+    @ExceptionHandler(BindException.class)
+    public Result<Void> handleBindException(BindException e) {
+        log.error(e.getMessage(), e);
+        String message = e.getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        return Result.fail(message);
+    }
+
+    /**
+     * 自定义验证异常
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public Result<Void> constraintViolationException(ConstraintViolationException e) {
+        log.error(e.getMessage(), e);
+        String message = e.getConstraintViolations().stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining(", "));
+        return Result.fail(message);
+    }
+
+    /**
+     * 自定义验证异常
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Result<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(e.getMessage(), e);
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+        return Result.fail(message);
+    }
+
 }
 
