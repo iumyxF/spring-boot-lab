23265d021788fcde0b507a4930988436316e549b
iumyxF <1119759672@qq.com>
2023-02-27 14:09:41 +0800

    feat(websocket): ✨ 集成websocket，添加使用入门案例

---
+++ spring-boot-websocket/spring-boot-websocket-getting-started/src/main/java/com/example/WebSocketApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: websocket入门案例
+ * @Date 2023/2/27 14:02
+ * @Author fzy
+ */
+@SpringBootApplication
+public class WebSocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketApplication.class, args);
+    }
+}
---
+++ spring-boot-websocket/spring-boot-websocket-getting-started/src/main/java/com/example/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @description:
+ * @Date 2023/2/27 14:02
+ * @Author fzy
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
