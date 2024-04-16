b3e197122ed39bfb718dcb829a507a117be7288f
iumyxF <1119759672@qq.com>
2024-04-16 16:40:49 +0800

    perf(spring-boot-translation): 1. 优化ArrayList存在的并发问题 2. 简化main函数,合理划分方法指责

--- spring-boot-translation/src/main/java/com/example/translation/TranslationApplication.java
+++ spring-boot-translation/src/main/java/com/example/translation/TranslationApplication.java
@@ -1,6 +1,5 @@
 package com.example.translation;
 
-import cn.hutool.core.date.DateUtil;
 import com.alibaba.excel.EasyExcel;
 import com.example.translation.dao.WordRepository;
 import com.example.translation.entities.LanguageConstant;
@@ -11,15 +10,12 @@ import com.example.translation.service.impl.AliTranslationServiceImpl;
 import com.google.common.collect.Lists;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author fzy
@@ -36,23 +32,49 @@ public class TranslationApplication {
      * 2. 按照步骤获取ak、sk
      * 3. 进入AliTranslationServiceImpl.class 填写ak、sk
      */
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //读取
+    public static void main(String[] args) {
+        // 读取未翻译单词数据
         String fileName = "F:\\testData\\translate\\test.xlsx";
-        EasyExcel.read(Files.newInputStream(Paths.get(fileName)), Word.class, new WordListener()).sheet().doRead();
+        List<Word> wordList = readByExcel(fileName);
+        // 多线程翻译
+        ConcurrentLinkedQueue<Word> resWords = doTranslate(wordList);
+        // 写入新的excel
+        String outputFileName = generateOutputFileName(fileName);
+        writeExcel(outputFileName, resWords);
+    }
 
-        //获取
+    /**
+     * 读取excel
+     *
+     * @param fileName 文件全路径
+     * @return 单词数据
+     */
+    public static List<Word> readByExcel(String fileName) {
         WordRepository repository = WordRepository.getInstance();
-        List<Word> wordList = repository.getAll();
-        int size = wordList.size();
-        System.out.println(size);
+        try (InputStream inputStream = Files.newInputStream(Paths.get(fileName))) {
+            EasyExcel.read(inputStream, Word.class, new WordListener()).sheet().doRead();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return repository.getAll();
+    }
 
-        //多线程翻译
-        ArrayList<Word> resWords = new ArrayList<>();
-        List<List<Word>> partition = Lists.partition(wordList, 50);
+    /**
+     * 多线程进行翻译
+     *
+     * @param wordList 带翻译单词列表
+     * @return 已翻译单词列表
+     */
+    private static ConcurrentLinkedQueue<Word> doTranslate(List<Word> wordList) {
+        // 保存最后结果
+        ConcurrentLinkedQueue<Word> resWords = new ConcurrentLinkedQueue<>();
+        // 创建翻译类,如果使用Spring直接注入即可
         TranslationService service = new AliTranslationServiceImpl();
+        // 按照50个单词为一组进行翻译
+        List<List<Word>> partition = Lists.partition(wordList, 50);
         ExecutorService threadPool = Executors.newFixedThreadPool(partition.size());
         List<CompletableFuture<Boolean>> futures = new ArrayList<>(partition.size());
+
         for (List<Word> requests : partition) {
             CompletableFuture<Boolean> future = CompletableFuture.supplyAsync(() -> {
                 List<Word> response = service.getBatchTranslate(requests, LanguageConstant.CHINESE, LanguageConstant.ENGLISH);
@@ -61,16 +83,39 @@ public class TranslationApplication {
             }, threadPool);
             futures.add(future);
         }
-        futures.forEach(CompletableFuture::join);
-        //写入
-        String fileName2 = "F:\\testData\\translate\\" + DateUtil.format(new Date(), "yyyyMMddHHmmss") + ".xlsx";
-        EasyExcel.write(fileName2, Word.class)
-                .sheet(LanguageConstant.ENGLISH)
-                .doWrite(resWords);
-
-        // 关闭线程池
+        //同步阻塞等待结果
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        //因为使用main函数执行,需要手动关闭线程池才能正常结束,如果使用web容器则不需要关闭
         threadPool.shutdown();
-        threadPool.awaitTermination(120, TimeUnit.NANOSECONDS);
+        try {
+            threadPool.awaitTermination(120, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        return resWords;
     }
 
+    /**
+     * 写入excel
+     *
+     * @param outputFileName 输出文件全路径
+     * @param resWords       翻译结果
+     */
+    private static void writeExcel(String outputFileName, ConcurrentLinkedQueue<Word> resWords) {
+        EasyExcel.write(outputFileName, Word.class)
+                .sheet(LanguageConstant.ENGLISH)
+                .doWrite(resWords);
+    }
+
+    /**
+     * 生成翻译文件名
+     *
+     * @param inputFileName 读取的文件名
+     * @return 结果
+     */
+    private static String generateOutputFileName(String inputFileName) {
+        String baseName = Paths.get(inputFileName).getFileName().toString();
+        String outputFileName = baseName.replace(".xlsx", "") + "_translated.xlsx";
+        return "F:\\testData\\translate\\" + outputFileName;
+    }
 }
