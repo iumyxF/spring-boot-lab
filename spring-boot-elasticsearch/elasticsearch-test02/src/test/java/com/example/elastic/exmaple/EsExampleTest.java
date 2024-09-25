10268bf6bf7042a8d9e2fb245823e98f9b0a17dc
iumyxF <1119759672@qq.com>
2024-09-25 17:32:03 +0800

    feat(spring-boot-elasticsearch): 增加es的api测试

--- spring-boot-elasticsearch/elasticsearch-test02/src/test/java/com/example/elastic/exmaple/EsExampleTest.java
+++ spring-boot-elasticsearch/elasticsearch-test02/src/test/java/com/example/elastic/exmaple/EsExampleTest.java
@@ -1,16 +1,18 @@
 package com.example.elastic.exmaple;
 
+import cn.hutool.core.collection.CollUtil;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.GeoDistanceType;
 import co.elastic.clients.elasticsearch._types.mapping.Property;
 import co.elastic.clients.elasticsearch._types.mapping.TypeMapping;
-import co.elastic.clients.elasticsearch.core.CreateResponse;
-import co.elastic.clients.elasticsearch.core.DeleteResponse;
-import co.elastic.clients.elasticsearch.core.GetResponse;
-import co.elastic.clients.elasticsearch.core.UpdateResponse;
+import co.elastic.clients.elasticsearch.core.*;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.elasticsearch.core.search.HitsMetadata;
 import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
 import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
 import co.elastic.clients.elasticsearch.indices.DeleteIndexRequest;
 import co.elastic.clients.elasticsearch.indices.DeleteIndexResponse;
+import co.elastic.clients.json.JsonData;
 import co.elastic.clients.transport.endpoints.BooleanResponse;
 import com.example.elastic.model.Hotel;
 import com.example.elastic.model.HotelDoc;
@@ -22,6 +24,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author fzy
@@ -35,7 +38,7 @@ class EsExampleTest {
     @Autowired
     ElasticsearchClient client;
     @Autowired
-    private HotelService hotelService;
+    HotelService hotelService;
 
     //region 索引操作
 
@@ -57,17 +60,22 @@ class EsExampleTest {
                         .index("hotel")
                         .mappings(new TypeMapping.Builder()
                                 .properties("id", Property.of(pBuilder -> pBuilder.keyword(kBuilder -> kBuilder.index(true))))
-                                .properties("name", Property.of(pBuilder -> pBuilder.text(kBuilder -> kBuilder.index(true).analyzer("smartcn").copyTo(searchWord))))
+                                .properties("name", Property.of(pBuilder -> pBuilder.text(kBuilder -> kBuilder.index(true)
+                                        .analyzer("smartcn")
+                                        .copyTo(searchWord))))
                                 .properties("address", Property.of(pBuilder -> pBuilder.keyword(kBuilder -> kBuilder.index(true))))
                                 .properties("price", Property.of(pBuilder -> pBuilder.integer(kBuilder -> kBuilder.index(true))))
                                 .properties("score", Property.of(pBuilder -> pBuilder.integer(kBuilder -> kBuilder.index(true))))
-                                .properties("brand", Property.of(pBuilder -> pBuilder.keyword(kBuilder -> kBuilder.index(true).copyTo(searchWord))))
+                                .properties("brand", Property.of(pBuilder -> pBuilder.keyword(kBuilder -> kBuilder.index(true)
+                                        .copyTo(searchWord))))
                                 .properties("city", Property.of(pBuilder -> pBuilder.keyword(kBuilder -> kBuilder.index(true))))
                                 .properties("starName", Property.of(pBuilder -> pBuilder.keyword(kBuilder -> kBuilder.index(true))))
-                                .properties("business", Property.of(pBuilder -> pBuilder.keyword(kBuilder -> kBuilder.index(true).copyTo(searchWord))))
+                                .properties("business", Property.of(pBuilder -> pBuilder.keyword(kBuilder -> kBuilder.index(true)
+                                        .copyTo(searchWord))))
                                 .properties("location", Property.of(pBuilder -> pBuilder.geoPoint(kBuilder -> kBuilder.index(true))))
                                 .properties("pic", Property.of(pBuilder -> pBuilder.keyword(kBuilder -> kBuilder.index(false))))
-                                .properties("search_word", Property.of(pBuilder -> pBuilder.text(kBuilder -> kBuilder.index(true).analyzer("smartcn"))))
+                                .properties("search_word", Property.of(pBuilder -> pBuilder.text(kBuilder -> kBuilder.index(true)
+                                        .analyzer("smartcn"))))
                                 .build())
                         .build());
         System.out.println(response.toString());
@@ -103,6 +111,142 @@ class EsExampleTest {
         System.out.println(response);
     }
 
+    /**
+     * match_all: 查询全部文档
+     */
+    @Test
+    public void queryMatchAll() throws IOException {
+        SearchResponse<HotelDoc> searchResponse = client.search(s -> s
+                        .index("hotel")
+                        .query(q -> q.matchAll(v -> v))
+                , HotelDoc.class);
+        printResult(searchResponse);
+    }
+
+    /**
+     * match: 分词模糊查询，针对text类型，类似mysql的 like查询
+     * {"query":{"match":{"search_word":"文"}}}
+     */
+    @Test
+    public void queryMatch() throws IOException {
+        SearchResponse<HotelDoc> searchResponse = client.search(s -> s
+                        .index("hotel")
+                        .query(q -> q
+                                .match(m -> m
+                                        //.field("name")
+                                        .field("search_word")
+                                        .query("文")))
+                , HotelDoc.class);
+        printResult(searchResponse);
+    }
+
+    /**
+     * 多条件查询
+     * {"query":{"multi_match":{"query":"文","fields":["name","brand","business"]}}}
+     * 注意：因为字段上面没有进行分词，所以可能出现的结果和search_word不同
+     */
+    @Test
+    public void queryMultiMatch() throws IOException {
+        SearchResponse<HotelDoc> searchResponse = client.search(s -> s
+                        .index("hotel")
+                        .query(q -> q
+                                .multiMatch(m -> m
+                                        .query("文")
+                                        .fields("name", "brand", "business")
+                                ))
+                , HotelDoc.class);
+        printResult(searchResponse);
+    }
+
+    /**
+     * 精确查询 term,针对keyword类型，类似mysql的 brand = 'xxx'
+     * 就算查询text分词，也不会模糊匹配
+     */
+    @Test
+    public void queryTerm() throws IOException {
+        SearchResponse<HotelDoc> searchResponse = client.search(s -> s
+                        .index("hotel")
+                        .query(q -> q
+                                .term(t -> t
+                                        // brand - keyword
+                                        //.field("brand")
+                                        // name -text
+                                        .field("name")
+                                        .value("广东省")))
+                , HotelDoc.class);
+        printResult(searchResponse);
+    }
+
+    @Test
+    public void queryRange() throws IOException {
+        SearchResponse<HotelDoc> searchResponse = client.search(s -> s
+                        .index("hotel")
+                        .query(q -> q
+                                .range(r -> r.field("price")
+                                        .lte(JsonData.of(1296016577))
+                                        .gte(JsonData.of(1206915764))
+                                ))
+                , HotelDoc.class);
+        printResult(searchResponse);
+    }
+
+    /**
+     * 坐标矩阵查询，根据左上角和右下角筛选一个矩形范围
+     */
+    @Test
+    public void queryGeoByBox() throws IOException {
+        SearchResponse<HotelDoc> searchResponse = client.search(s -> s
+                        .index("hotel")
+                        .query(q -> q
+                                .geoBoundingBox(g -> g
+                                        .field("location")
+                                        .boundingBox(b -> b
+                                                .tlbr(t -> t
+                                                        // 经度:109.5702918030936 纬度:42.81018784349334
+                                                        .topLeft(tl -> tl
+                                                                .latlon(lat -> lat
+                                                                        .lon(109.5702918030936)
+                                                                        .lat(42.81018784349334)
+                                                                ))
+                                                        // 经度:118.14915253959975 纬度:21.739743055059776
+                                                        .bottomRight(br -> br
+                                                                .latlon(lat -> lat
+                                                                        .lon(118.14915253959975)
+                                                                        .lat(21.739743055059776)
+                                                                ))
+                                                )
+                                        )
+                                )
+                        )
+                , HotelDoc.class);
+        printResult(searchResponse);
+    }
+
+    /**
+     * 圆心查询 查询某个坐标附近 x km范围的信息
+     */
+    @Test
+    public void queryGeoByCircle() throws IOException {
+        SearchResponse<HotelDoc> response = client.search(s -> s
+                        .index("hotel")
+                        .query(q -> q
+                                .bool(b -> b
+                                        .filter(f -> f
+                                                .geoDistance(g -> g
+                                                        .distance("1700km")
+                                                        .field("location")
+                                                        .location(l -> l
+                                                                .latlon(v -> v
+                                                                        .lat(36.30556423523153)
+                                                                        .lon(104.48060937499996)
+                                                                ))
+                                                        .distanceType(GeoDistanceType.Arc)
+                                                ))
+                                ))
+                , HotelDoc.class);
+        printResult(response);
+    }
+
     @Test
     public void insertDoc() throws IOException {
         Hotel hotel = hotelService.getById(1L);
@@ -113,12 +257,40 @@ class EsExampleTest {
         System.out.println(response);
     }
 
+    /**
+     * 如果某个id存在文档 会直接更新
+     */
+    @Test
+    public void batchInsertDoc() throws IOException {
+        List<Hotel> hotels = hotelService.list();
+        BulkRequest.Builder br = new BulkRequest.Builder();
+        for (Hotel hotel : hotels) {
+            br.operations(op -> op
+                    .index(idx -> idx
+                            .index("hotel")
+                            .id(String.valueOf(hotel.getId()))
+                            .document(new HotelDoc(hotel))
+                    )
+            );
+        }
+        BulkResponse response = client.bulk(br.build());
+        System.out.println(response);
+    }
+
     @Test
     public void delDocById() throws IOException {
         DeleteResponse response = client.delete(builder -> builder
                 .index("hotel")
                 .id(String.valueOf(1)));
         System.out.println(response);
+    }
+
+    @Test
+    public void delAllDoc() throws IOException {
+        DeleteByQueryResponse deleteByQueryResponse = client.deleteByQuery(d -> d
+                .index("hotel")
+                .query(q -> q.matchAll(v -> v)));
+        System.out.println(deleteByQueryResponse);
     }
 
     /**
@@ -137,4 +309,14 @@ class EsExampleTest {
     }
 
     //endregion
+
+    private void printResult(SearchResponse<HotelDoc> searchResponse) {
+        List<Hit<HotelDoc>> hitList = Optional.of(searchResponse)
+                .map(SearchResponse::hits)
+                .map(HitsMetadata::hits)
+                .orElse(null);
+        if (CollUtil.isNotEmpty(hitList)) {
+            hitList.forEach(h -> System.out.println(h.source()));
+        }
+    }
 }
