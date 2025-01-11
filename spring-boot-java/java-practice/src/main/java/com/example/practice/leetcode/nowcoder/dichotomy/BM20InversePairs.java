193dbefba6535b8090475ca1e50d41b39981e66a
iumyxF <1119759672@qq.com>
2025-01-11 17:25:38 +0800

    feat(java-practice): BM20InversePairs

--- spring-boot-java/java-practice/src/main/java/com/example/practice/leetcode/nowcoder/dichotomy/BM20InversePairs.java
+++ spring-boot-java/java-practice/src/main/java/com/example/practice/leetcode/nowcoder/dichotomy/BM20InversePairs.java
@@ -23,8 +23,78 @@ public class BM20InversePairs {
     输入: [1,2,3]
     输出: 0
      */
-    public int inversePairs(int[] nums) {
 
-        return 0;
+    /**
+     * 暴力
+     * 超时了
+     */
+    public int inversePairsTimeout(int[] nums) {
+        if (nums.length < 2) {
+            return 0;
+        }
+        int res = 0;
+        int maxValue = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // 剪枝
+            if (nums[i] > maxValue) {
+                maxValue = nums[i];
+                continue;
+            }
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] < nums[j]) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    int res = 0;
+
+    public int inversePairs(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return res;
+    }
+
+    public void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        doSort(arr, left, mid, right);
+    }
+
+    private void doSort(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int index = 0;
+        int l = left;
+        int r = mid + 1;
+        while (l <= mid && r <= right) {
+            if (arr[l] <= arr[r]) {
+                temp[index++] = arr[l++];
+            } else {
+                // mid - l + 1 ==> 左区间内，有多少个大于arr[l]的元素,他们都大于arr[r]，所以是逆序对
+                res = (res + (mid - l + 1)) % 1000000007;
+                temp[index++] = arr[r++];
+            }
+        }
+        while (l <= mid) {
+            temp[index++] = arr[l++];
+        }
+        while (r <= right) {
+            temp[index++] = arr[r++];
+        }
+        for (int k = 0; k < temp.length; k++) {
+            arr[left + k] = temp[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        BM20InversePairs s = new BM20InversePairs();
+        //System.out.println(s.inversePairs(new int[]{1, 2, 3, 4, 5, 6, 7, 0}));
+        //System.out.println(s.inversePairs(new int[]{1, 2, 3}));
+        System.out.println(s.inversePairs(new int[]{1, 8, 4, 7, 2}));
     }
 }
